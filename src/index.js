413a1e8fb9571b32ff75238b2c1399fac3231057
曹俊 <619635878@qq.com>
2018-07-06 14:54:15 +0800

    修改readme.md

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.sass';
-import App from './App';
+// import App from './App';
 import Layout from './layout/Layout'
 // import Layout from './layout/Layout.js'
 import registerServiceWorker from './registerServiceWorker';
