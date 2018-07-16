9598433a548f2fb3fb3877836c1c63812f086171
曹俊 <619635878@qq.com>
2018-07-16 10:23:56 +0800

    更新一下

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.sass';
-// import App from './App';
 import Layout from './layout/Layout'
 import registerServiceWorker from './registerServiceWorker';
-// console.log(<App/>)
 ReactDOM.render(
   <Layout/>,
   document.getElementById('root')
