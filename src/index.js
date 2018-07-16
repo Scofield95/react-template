c72be7e9dc721f7cdcdb74fe2896ba333f2b9942
曹俊 <619635878@qq.com>
2018-07-16 18:09:02 +0800

    更新一下

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.sass';
 import Layout from './layout/Layout'
+import { createStore } from 'redux'
 import registerServiceWorker from './registerServiceWorker';
+console.log(createStore)
 ReactDOM.render(
   <Layout/>,
   document.getElementById('root')
